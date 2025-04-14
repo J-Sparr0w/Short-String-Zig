7d625f87d7f6519463924191411aa2bc3a1dd007
Arjun H <18arjunh@gmail.com>
2025-04-14 22:54:32 +0530

    first commit

---
+++ build.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // LIBRARY Module Creation:
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "short_string",
+        .root_module = lib_mod,
+    });
+
+    b.installArtifact(lib);
+
+    // const exe_mod = b.createModule(.{
+    //     // `root_source_file` is the Zig "entry point" of the module. If a module
+    //     // only contains e.g. external object files, you can make this `null`.
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // const exe = b.addExecutable(.{
+    //     .name = "short_str",
+    //     .root_module = exe_mod,
+    // });
+
+    // b.installArtifact(exe);
+
+    // const run_cmd = b.addRunArtifact(exe);
+
+    // run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+
+    // const run_step = b.step("run", "Run the application");
+    // run_step.dependOn(&run_cmd.step);
+
+    // TESTS
+
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
---
+++ src/lib.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+const testing = std.testing;
+
+const String = struct {
+    len: u32,
+    prefix: @Vector(4, u8),
+    rest: packed union {
+        trailing: @Vector(8, u8),
+        ptr: [*]u8,
+    },
+
+    pub fn from_str(allocator: std.mem.Allocator, str: []const u8) !String {
+        const len = str.len;
+        if (len > std.math.maxInt(u32)) {
+            return error.stringTooLong;
+        }
+
+        if (len <= 12) {
+            var trailing: [8]u8 = .{0} ** 8;
+            for (str[4..], 0..) |ch, i| {
+                trailing[i] = ch;
+            }
+            // add to trailing
+            return String{
+                .len = @intCast(len),
+                .prefix = str[0..4].*,
+                .rest = .{
+                    .trailing = trailing,
+                },
+            };
+        } else {
+            // add to heap
+            const heap_str = try allocator.alloc(u8, len);
+            std.mem.copyForwards(u8, heap_str, str[0..]);
+            return String{
+                .len = @intCast(len),
+                .prefix = str[0..4].*,
+                .rest = .{
+                    .ptr = heap_str.ptr,
+                },
+            };
+        }
+    }
+    pub fn is_heap_allocated(self: String) bool {
+        return self.len > 12;
+    }
+    pub fn get_heap_ptr(self: String) ?[*]u8 {
+        if (is_heap_allocated(self)) {
+            return self.rest.ptr;
+        } else {
+            return null;
+        }
+    }
+    // pub fn deinit(self: String) void {
+    //     self.len = 0;
+    //     self.prefix = [_]u8{0} ** 8;
+    //     if (self.len > 12) {
+    //         std.ArrayList(comptime T: type)
+    //         (self.rest.ptr);
+    //     }
+    // }
+    pub fn format(
+        self: String,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+        const prefix: [4]u8 = self.prefix;
+
+        try writer.print("String {{\nLen: {}\nPrefix: `{s}`", .{
+            self.len, prefix,
+        });
+
+        if (self.len <= 12) {
+            const trailing: [8]u8 = self.rest.trailing;
+            try writer.print("\nRest (on the stack): `{s}`", .{trailing});
+        } else {
+            try writer.print("\nRest (on the heap): {s}", .{self.rest.ptr[0..self.len]});
+        }
+
+        try writer.writeAll("\n}");
+    }
+};
+
+pub fn main() void {
+    std.log("Size of String: {}", .{@sizeOf(String)});
+}
+
+test "basic test functionality" {
+    const allocator = std.testing.allocator;
+    const short_str = try String.from_str(allocator, "short");
+    const long_str = try String.from_str(allocator, "a long string");
+    std.debug.print("\nSize of String: {}", .{@sizeOf(String)});
+    std.debug.print("\nAlignment of String: {}", .{@alignOf(String)});
+    std.debug.print("\nString: {s}", .{short_str});
+    std.debug.print("\nString: {s}", .{long_str});
+
+    try testing.expect(10 == 10);
+    std.debug.print("\nTest Passed Successfully", .{});
+    if (long_str.get_heap_ptr()) |ptr| {
+        allocator.free(ptr);
+    }
+}
